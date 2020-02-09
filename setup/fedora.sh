18716d97327cb41de723458cb688c7cd9faabb2a
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2020-02-09 16:23:39 +0000

    setup: fedora: Initial setup script for fedora RawHide
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

---
+++ setup/fedora.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Script to setup an Android 10 build
+# environment for Fedora 31 / Rawhide.
+# Bash Colors
+# red = errors, cyan = warnings, green = confirmations, blue = informational
+# plain for generic text, bold for titles, reset flag at each end of line
+# plain blue should not be used for readability reasons - use plain cyan instead
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_RED=$CLR_RST$(tput setaf 1)             #  red, plain
+CLR_GRN=$CLR_RST$(tput setaf 2)             #  green, plain
+CLR_YLW=$CLR_RST$(tput setaf 3)             #  yellow, plain
+CLR_BLU=$CLR_RST$(tput setaf 4)             #  blue, plain
+CLR_PPL=$CLR_RST$(tput setaf 5)             #  purple,plain
+CLR_CYA=$CLR_RST$(tput setaf 6)             #  cyan, plain
+CLR_BLD=$(tput bold)                        ## bold flag
+CLR_BLD_RED=$CLR_RST$CLR_BLD$(tput setaf 1) #  red, bold
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_YLW=$CLR_RST$CLR_BLD$(tput setaf 3) #  yellow, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+CLR_BLD_PPL=$CLR_RST$CLR_BLD$(tput setaf 5) #  purple, bold
+CLR_BLD_CYA=$CLR_RST$CLR_BLD$(tput setaf 6) #  cyan, bold
+
+# Packages
+sudo dnf install -y \
+    autoconf213 \
+    bison \
+    bzip2 \
+    ccache \
+    curl \
+    flex \
+    gawk \
+    gcc-c++ \
+    git \
+    glibc-devel \
+    glibc-static \
+    libstdc++-static \
+    libX11-devel \
+    make \
+    mesa-libGL-devel \
+    ncurses-devel \
+    patch \
+    zlib-devel \
+    ncurses-devel.i686 \
+    readline-devel.i686 \
+    zlib-devel.i686 \
+    libX11-devel.i686 \
+    mesa-libGL-devel.i686 \
+    glibc-devel.i686 \
+    libstdc++.i686 \
+    libXrandr.i686 \
+    zip \
+    perl-Digest-SHA \
+    python2 \
+    wget \
+    lzop \
+    openssl-devel \
+    java-1.8.0-openjdk-devel \
+    ImageMagick \
+    ncurses-compat-libs \
+    schedtool \
+    lzip \
+    vboot-utils
+
+# The package libncurses5 is not available, so we need to hack our way by symlinking the required library.
+sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libncurses.so.5
+sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libtinfo.so.5
+
+# For all those distro hoppers, lets setup your git credentials
+GIT_USERNAME="$(git config --get user.name)"
+GIT_EMAIL="$(git config --get user.email)"
+echo -e "${CLR_BLD_BLU}Configuring Git${CLR_RST}"
+if [[ -z ${GIT_USERNAME} ]]; then
+	echo -e "${CLR_BLD_RED}Enter your name: ${CLR_RST}"
+	read -r NAME
+	git config --global user.name "${NAME}"
+fi
+if [[ -z ${GIT_EMAIL} ]]; then
+	echo -e "${CLR_BLD_RED}Enter your email: ${CLR_RST}"
+	read -r EMAIL
+	git config --global user.email "${EMAIL}"
+fi
+git config --global credential.helper "cache --timeout=7200"
+echo -e "${CLR_BLD_GRN}Setting-up Github credentials setup successfully${CLR_RST}"
+echo -e ""
+git config --global alias.cp 'cherry-pick'
+git config --global alias.cpn 'cherry-pick -n'
+git config --global alias.c 'commit'
+git config --global alias.f 'fetch'
+
+# Repo
+echo "Installing Git Repository Tool"
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx /usr/local/bin/repo
+
+echo -e "Setting up udev rules for ADB!"
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+sudo chmod 644 /etc/udev/rules.d/51-android.rules
+sudo chown root /etc/udev/rules.d/51-android.rules
+sudo udevadm control --reload-rules
